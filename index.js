c3f59d9da774099bbd2dccca049bdc3526babe6a
DivyaUtla <divyachowdaryb0@gmail.com>
2024-06-02 22:28:15 -0400

    Adding the project REPO

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+app.use(express.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+})
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res) {req.sendFile(path.join(__dirname,"/public","index.html"));});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
